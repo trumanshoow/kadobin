36b901e8fe26b147cae6c895768ff63e4437e2ad
trumanshoow <dehghanalireza1214@gmail.com>
2023-12-02 16:56:05 +0330

    heeey

--- src/assets/icons/sim_card_icon.js
+++ src/assets/icons/sim_card_icon.js
@@ -1,11 +1,7 @@
 const Sim_card_icon = (props) => {
     return (
         <>
-<<<<<<< HEAD
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 21.856 27.32" {...props}>
-=======
-            <svg xmlns="http://www.w3.org/2000/svg" width="28.856" height="27.32" viewBox="0 0 21.856 27.32">
->>>>>>> 5cf147eeea99a0d389627dcb6e15841ab7059f82
                 <g transform="translate(-4 -2)">
                     <path
                         d="M4,25.222V6.1A4.1,4.1,0,0,1,8.1,2h8.665a5.458,5.458,0,0,1,3.863,1.6L24.255,7.23a5.458,5.458,0,0,1,1.6,3.863V25.222a4.1,4.1,0,0,1-4.1,4.1H8.1A4.1,4.1,0,0,1,4,25.222Z"
