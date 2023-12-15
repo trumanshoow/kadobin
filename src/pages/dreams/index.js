b8c61836f9331b9a8cc86cb232814da146463332
Developer <Master.Developer23@gmail.com>
2023-12-15 14:43:44 +0330

    make dream page !!!

--- src/pages/dreams/index.js
+++ src/pages/dreams/index.js
@@ -1,7 +1,16 @@
+import Styles from "./dream.module.css"
+import Button from "../../components/button";
 const DreamPage = () => {
     return (
         <>
+            <img className={Styles.picture} src={require("../../assets/image/Ghool cheragh jadoo.png")} alt="Ghool cheragh jadoo"/>
 
+            <Button text="آرزو کن" className={Styles.Btn}></Button>
+            <div className={Styles.subtitle}>
+                <span>یادت باشه که آرزو هات قراره توسط کسایی که بهت می خوان کادو بدن قراره برآورده بشه ، پس چیزی آرزو نکن که از توانشون خارج</span>
+            </div>
+
+            <button className={Styles.dreamCounter}>هنوز 3 تا آرزوی دیگه داری</button>
         </>
     );
 };
