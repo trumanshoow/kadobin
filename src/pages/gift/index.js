f6f104a35c5259acb95d5dd37d40163097d0c053
alidev9831 <alidev9831@gmail.com>
2023-12-20 10:07:49 +0330

    add gift and shop pages

--- src/pages/gift/index.js
+++ src/pages/gift/index.js
@@ -1,12 +1,29 @@
 import Styles from './gift.module.css'
+import Input from '../../components/input/index'
+import { text } from '@fortawesome/fontawesome-svg-core';
+import Telephon from '../../assets/icons/telephon'
+import Button from '../../components/button'
+import Back from '../../assets/icons/back-card-icon'
 
 const GiftPage = () => {
     return (
-        <>
-            <div className={Styles.KadoImg}>
-                <img src={require("../../assets/image/kado.png")} alt="kado" />
+        <div className={Styles.body}>
+            <div className={Styles.white}>
+                <div className={Styles.container}>
+                    <img className={Styles.img} src={require("../../assets/image/kado.png")} alt="kado" />
+                    <h3 className={Styles.title}> به کی می خوای هدیه بدی ؟ </h3>
+                    <div>
+                        <Input className={Styles.Input} type={text} placeholder=" نام و نام خانوادگی " />
+                    </div>
+                    <div className={Styles.parentinput}>
+                        <Input className={Styles.Input2} type={Number} placeholder=" 98 _ _ _ _ _ _ _ _ _ _ _ " />
+                        <button className={Styles.button}><Telephon className={Styles.Telephon}/></button>
+                    </div>
+                    <h3 className={Styles.name}> اسم و شماره تماس کسی که می خوای بهش کادو بدی رو وارد کن </h3>
+                    <Button className={Styles.Button} text="ادامه"></Button>
+                </div>
             </div>
-        </>
+        </div>
     )
 }
 export default GiftPage;
--- src/pages/shop/index.js
+++ src/pages/shop/index.js
@@ -1,13 +1,78 @@
 import React from 'react';
-import BottomNavigation from "../../components/bottomNavigation";
+import Styles from './shop.module.css'
+import HamburgMenu_icon from '../../assets/icons/hamburgMenuIcon'
+import Banknotes from '../../assets/icons/BanknotesCardIcon'
+import Christmas from '../../assets/icons/ChristmasPenguinCardIcon'
+import Flower from '../../assets/icons/flowerCardIcon'
+import Jewelry from '../../assets/icons/jewelryCardIcon'
+import Smartphone from '../../assets/icons/SmartphoneCardIcon'
+import Soccer from '../../assets/icons/soccerBallCardIcon'
+import Souvenirs from '../../assets/icons/souvenirsCardIcon'
+import Tshirt from '../../assets/icons/tShirtCardIcon'
+import Women from '../../assets/icons/womensShoeCardIcon'
+import Button from '../../components/button/index'
+import ServicesButton2 from '../../components/ServicesButton2'
+
+
+const data =[
+   {test:" 68 ", text:" پوشاک مردانه ", Icon:Tshirt},
+   { test:" 25 ", text:" زیورالات ", Icon:Jewelry},
+   { test:" 25 ", text:" هدیه نقدی ", Icon:Banknotes},
+   { test:" 59 ", text:" اسباب بازی ", Icon:Christmas},
+   { test:" 325 ", text:" کالای دیجیتال " ,Icon:Smartphone},
+   { test:" 25 ", text:" وسایل ورزشی ", Icon:Soccer},
+   { test:" 25 ", text:" رز جاودان ", Icon:Flower},
+   { test:" 630 ", text:" اکسسوری ", Icon:Souvenirs},
+   { test:" 100 ", text:" کیف و کفش ", Icon:Women},
+]
 
 const ShopPage = () => {
-    return (
-        <>
-            <h1>im ShopPage</h1>
-            <BottomNavigation/>
-        </>
-    );
+
+   return (
+      <>
+         <div className={Styles.container}>
+            <div className={Styles.header}>
+                        <HamburgMenu_icon className={Styles.menu}/>
+                    <div>
+                        <h3> فروشگاه کادوبین </h3>
+                        <span> انتخاب دسته بندی </span>
+                    </div>
+                </div>
+                <div className={Styles.boxWrapper}>
+                  {
+                     data.map((item , index) => {
+                        <ServicesButton2 //className={Styles.ServicesButton2} 
+                           key={index}
+                           {...item}
+                        />
+                     })
+                  }
+
+            {/* <ServicesButton2 className={Styles.ServicesButton2}
+            />
+            <ServicesButton2 className={Styles.ServicesButton2}
+            /> */}
+            </div>
+            {/* <div className={Styles.group}>
+            <ServicesButton2 className={Styles.ServicesButton2}
+            />
+            <ServicesButton2 className={Styles.ServicesButton2}
+            />
+            <ServicesButton2 className={Styles.ServicesButton2}
+            />
+            </div>
+            <div className={Styles.group}>
+            <ServicesButton2 className={Styles.ServicesButton2}
+            />
+            <ServicesButton2 className={Styles.ServicesButton2}
+            />
+            <ServicesButton2 className={Styles.ServicesButton2}
+            />
+            </div> */}
+            <Button className={Styles.Button} text=" همه محصولات " />
+         </div>
+      </>
+   );
 };
 
 export default ShopPage;
