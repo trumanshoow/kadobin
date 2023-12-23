b95f0d651328656191cc52223c977aadcfdf9d04
trumanshoow <dehghanalireza1214@gmail.com>
2023-12-23 15:28:37 +0330

    Edit

--- src/assets/icons/dreams_icon.js
+++ src/assets/icons/dreams_icon.js
@@ -1,4 +1,3 @@
-import React from 'react';
 
 const DreamsIcon = ({width ,className}) => {
     return (
--- src/pages/gift/index.js
+++ src/pages/gift/index.js
@@ -6,21 +6,21 @@ import Button from '../../components/button'
 const GiftPage = () => {
     return (
         <div className={Styles.body}>
-            <div className={Styles.white}>
+            <div className={Styles.whiteSection}>
                 <div className={Styles.container}>
-                    <img className={Styles.img} src={require("../../assets/image/kado.png")} alt="kado" />
+                    <img className={Styles.kadoImg} src={require("../../assets/image/kado.png")} alt="kado" />
                     <h3 className={Styles.title}> به کی می خوای هدیه بدی ؟ </h3>
                     <div>
                         <Input className={Styles.Input} type={text} placeholder=" نام و نام خانوادگی " />
                     </div>
-                    <div className={Styles.parentinput}>
+                    <div className={Styles.parentInput}>
                         <Input className={Styles.Input2} type={Number} placeholder=" 98 _ _ _ _ _ _ _ _ _ _ _ "  />
-                        <button className={Styles.button}><Telephon className={Styles.Telephon}/></button>
+                        <button className={Styles.button}><Telephon className={Styles.Telephone}/></button>
                     </div>
                     <h3 className={Styles.name}> اسم و شماره تماس کسی که می خوای بهش کادو بدی رو وارد کن </h3>
-                    <Button className={Styles.Button} text="ادامه"></Button>
                 </div>
             </div>
+                    <Button className={Styles.Button} text="ادامه"></Button>
         </div>
     )
 }
