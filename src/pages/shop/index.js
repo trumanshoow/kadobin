902bf505d015c2e9171cfcf72a4d5c300db64255
alidev9831 <alidev9831@gmail.com>
2023-12-25 11:54:21 +0330

    mamad

--- src/pages/shop/index.js
+++ src/pages/shop/index.js
@@ -12,34 +12,36 @@ import Women from '../../assets/icons/womensShoeCardIcon'
 import Button from '../../components/button/index'
 import ServicesButton from '../../components/LorgServicesButton'
 
-const data =[
-   { subTitle:" 68 ", title:" پوشاک مردانه ", Icon:Tshirt},
-   { subTitle:" 25 ", title:" زیورالات ", Icon:Jewelry},
-   { subTitle:" 25 ", title:" هدیه نقدی ", Icon:Banknotes},
-   { subTitle:" 59 ", title:" اسباب بازی ", Icon:Christmas},
-   { subTitle:" 325 ", title:" کالای دیجیتال " ,Icon:Smartphone},
-   { subTitle:" 25 ", title:" وسایل ورزشی ", Icon:Soccer},
-   { subTitle:" 25 ", title:" رز جاودان ", Icon:Flower},
-   { subTitle:" 630 ", title:" اکسسوری ", Icon:Souvenirs},
-   { subTitle:" 100 ", title:" کیف و کفش ", Icon:Women},
+const data = [
+   { subTitle: " 68 ", title: " پوشاک مردانه ", Icon: Tshirt },
+   { subTitle: " 25 ", title: " زیورالات ", Icon: Jewelry },
+   { subTitle: " 25 ", title: " هدیه نقدی ", Icon: Banknotes },
+   { subTitle: " 59 ", title: " اسباب بازی ", Icon: Christmas },
+   { subTitle: " 325 ", title: " کالای دیجیتال ", Icon: Smartphone },
+   { subTitle: " 25 ", title: " وسایل ورزشی ", Icon: Soccer },
+   { subTitle: " 25 ", title: " رز جاودان ", Icon: Flower },
+   { subTitle: " 630 ", title: " اکسسوری ", Icon: Souvenirs },
+   { subTitle: " 100 ", title: " کیف و کفش ", Icon: Women },
 ]
 
 const ShopPage = () => {
 
    return (
       <>
-         <div className={Styles.container}>
-                <div className={Styles.boxWrapper}>
+         <div className={Styles.body}>
+            <div className={Styles.container}>
+               <div className={Styles.boxWrapper}>
                   {
-                     data.map((item , index) => (
-                        <ServicesButton className={Styles.ServicesButton2} 
+                     data.map((item, index) => (
+                        <ServicesButton className={Styles.ServicesButton2}
                            key={index}
                            {...item}
                         />
                      ))
                   }
+               </div>
+               <Button className={Styles.Button} text=" همه محصولات " />
             </div>
-            <Button className={Styles.Button} text=" همه محصولات " />
          </div>
       </>
    );
