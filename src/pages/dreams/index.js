c927119d10fb20a63af66adc234fa552d5433579
trumanshoow <dehghanalireza1214@gmail.com>
2023-12-27 13:24:18 +0330

    ...

--- src/pages/dreams/index.js
+++ src/pages/dreams/index.js
@@ -1,12 +1,8 @@
 import React, {useEffect, useState} from "react";
 import {useNavigate} from "react-router-dom";
 import Button from "../../components/button";
-import Styles from "./dream.module.css";
-import {useDispatch} from "react-redux";
-import {setPageData} from "../../redux/slices/pageData";
+import styles from "./dream.module.css";
 import HamburgMenu_icon from "../../assets/icons/hamburgMenu_icon";
-import styles from "../dreamAdd/dreamAdd.module.css";
-import Back from "../../assets/icons/back-card-icon";
 import MainLayout from "../../layout/main";
 
 
@@ -21,25 +17,27 @@ const DreamPage = () => {
     return (
         <MainLayout
             title="آرزو ها"
+            titleProps={{className: styles.title}}
             subtitle=""
             headerColor="white"
             icon={<HamburgMenu_icon width="2.5rem"/>}
+
         >
             <>
-                <div className={Styles.all}>
-                    <div className={Styles.body}>
-                        <div className={Styles.section}>
-                            <div className={Styles.container}>
-                                <img className={Styles.picture} src={require("../../assets/image/Ghool cheragh jadoo.png")} alt="Ghool cheragh jadoo"/>
+                <div className={styles.all}>
+                    <div className={styles.body}>
+                        <div className={styles.section}>
+                            <div className={styles.container}>
+                                <img className={styles.picture} src={require("../../assets/image/Ghool cheragh jadoo.png")} alt="Ghool cheragh jadoo"/>
                             </div>
                         </div>
                     </div>
-                    <div className={Styles.container}>
-                        <Button onClick={()=> navigation("/dreams/addwish")} text="آرزو کن" className={Styles.Btn}></Button>
-                        <div className={Styles.subtitle}>
+                    <div className={styles.container}>
+                        <Button onClick={()=> navigation("/dreams/addwish")} text="آرزو کن" className={styles.Btn}></Button>
+                        <div className={styles.subtitle}>
                             <span>یادت باشه که آرزو هات قراره توسط کسایی که بهت می خوان کادو بدن قراره برآورده بشه ، پس چیزی آرزو نکن که از توانشون خارج</span>
                         </div>
-                        <button  className={Styles.dreamCounter}>هنوز <span className={Styles.valueSpan}>{value}</span> تا آرزوی دیگه داری</button>
+                        <button  className={styles.dreamCounter}>هنوز <span className={styles.valueSpan}>{value}</span> تا آرزوی دیگه داری</button>
                     </div>
 
                 </div>
