0a3bb886d65fc432889c62a83bd207d2ef6887d1
Developer-web2023 <Master.Developer23@gmail.com>
2023-12-27 15:06:06 +0330

    etgojro;ldfhm

---
+++ src/assets/icons/uploadPicture.js
@@ -0,0 +1,68 @@
+import React from 'react';
+
+const UploadPicture = () => {
+    return (
+        <div>
+            <svg xmlns="http://www.w3.org/2000/svg" width="39.993" height="43.992" viewBox="0 0 39.993 43.992">
+                <g id="Group_2372" data-name="Group 2372" transform="translate(-194 -213.004)">
+                    <g id="Group_2371" data-name="Group 2371">
+                        <g id="Group_2370" data-name="Group 2370">
+                            <g id="Group_2369" data-name="Group 2369">
+                                <g id="Group_2368" data-name="Group 2368">
+                                    <g id="Group_2367" data-name="Group 2367">
+                                        <g id="Group_2366" data-name="Group 2366">
+                                            <g id="Group_2365" data-name="Group 2365">
+                                                <g id="Group_2364" data-name="Group 2364">
+                                                    <g id="Group_2363" data-name="Group 2363">
+                                                        <g id="Group_2362" data-name="Group 2362">
+                                                            <g id="Group_2361" data-name="Group 2361">
+                                                                <g id="Group_2360" data-name="Group 2360">
+                                                                    <g id="Group_2359" data-name="Group 2359">
+                                                                        <g id="icons8_Image_File_Add"
+                                                                           transform="translate(190 211.004)">
+                                                                            <path id="Path_7953" data-name="Path 7953"
+                                                                                  d="M14,10V2L26,14H18A4,4,0,0,1,14,10Z"
+                                                                                  transform="translate(9.996)"
+                                                                                  fill="#5b5b5b"/>
+                                                                            <circle id="Ellipse_49"
+                                                                                    data-name="Ellipse 49" cx="3" cy="3"
+                                                                                    r="3"
+                                                                                    transform="translate(21.996 17.996)"
+                                                                                    fill="#5b5b5b"/>
+                                                                            <path id="Path_7954" data-name="Path 7954"
+                                                                                  d="M35.994,28.277V14L24,2H10A6,6,0,0,0,4,8V35.994a6,6,0,0,0,6,6H28.755a7.974,7.974,0,1,0,7.239-13.715Z"
+                                                                                  fill="#5b5b5b" opacity="0.35"/>
+                                                                            <path id="Path_7955" data-name="Path 7955"
+                                                                                  d="M24,14A10,10,0,1,0,34,24,10,10,0,0,0,24,14Zm5,12H26v3a2,2,0,0,1-4,0V26H19a2,2,0,0,1,0-4h3V19a2,2,0,0,1,4,0v3h3a2,2,0,0,1,0,4Z"
+                                                                                  transform="translate(9.996 11.996)"
+                                                                                  fill="#5b5b5b"/>
+                                                                            <g id="Group_1774" data-name="Group 1774"
+                                                                               transform="translate(9.999 23.996)">
+                                                                                <path id="Path_7956"
+                                                                                      data-name="Path 7956"
+                                                                                      d="M12,13c-2.5,0-5,7.451-5,10.748A1.254,1.254,0,0,0,8.25,25H17a13.933,13.933,0,0,1,1.59-6.465C16.292,17.133,14.077,13,12,13Z"
+                                                                                      transform="translate(-7 -13)"
+                                                                                      fill="#5b5b5b"/>
+                                                                            </g>
+                                                                        </g>
+                                                                    </g>
+                                                                </g>
+                                                            </g>
+                                                        </g>
+                                                    </g>
+                                                </g>
+                                            </g>
+                                        </g>
+                                    </g>
+                                </g>
+                            </g>
+                        </g>
+                    </g>
+                </g>
+            </svg>
+
+        </div>
+    );
+};
+
+export default UploadPicture;
--- src/pages/dreamAdd/index.js
+++ src/pages/dreamAdd/index.js
@@ -1,43 +1,32 @@
 import React, {useRef} from 'react';
 import Back from "../../assets/icons/back-card-icon";
+import Upload from "../../assets/icons/uploadPicture";
 import MainLayout from "../../layout/main";
 import styles from "./dreamAdd.module.css";
 const DreamAdd = () => {
 
     const inputRef = useRef(null);
 
-    const handleChange = event => {
-        if(event.currentTarget.files[0]?.size / 1024 / 1024 > 50) {
-            alert("این فایل بیش از اندازه بزرگ است");
-        }
-    }
-
     return (
         <MainLayout
-            icon={<Back width="2.5rem"/>}
             title="ثبت آرزو"
             subtitle="09032175822"
             headerColor="white"
+            icon={<Back width="2.5rem"/>}
         >
-            <div>
+            <div className={styles.container}>
+
                 <input
                     ref={inputRef}
-                    style={{display: "none" , className: styles}}
+                    className={styles.inp}
                     type="file"
-                    onChange={handleChange}
                 />
                 <button
-                    style={{
-                        backgroundColor: "#ccc",
-                        border: "1px solid grey",
-                        borderRadius: ".7rem",
-                        width: 250,
-                        height: 250
-
-                    }}
+                    className={styles.Btn}
                     onClick={() => inputRef.current.click()}
                 >
-                    Click me
+                    <Upload className={styles.upload}/>
+                    آپلود تصویر آرزو
                 </button>
             </div>
         </MainLayout>
